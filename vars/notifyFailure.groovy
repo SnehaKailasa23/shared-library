bb049f55af65681b7c1924f65e94b0f26c1225c1
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 19:06:08 +0530

    Update notifyFailure.groovy

--- vars/notifyFailure.groovy
+++ vars/notifyFailure.groovy
@@ -1,3 +1,4 @@
 def call(def Reason){
+  properties([[$class: 'EnvInjectJobProperty', info: [loadFilesFromMaster: false, propertiesContent: "Reason=${Reason}"], keepBuildVariables: true, keepJenkinsSystemVariables: true, on: true]])
   emailext (attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''${SCRIPT, template="email_template_failure.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com, sneha.kailasa@ggktech.com, sunil.boga@ggktech.com')
 }
