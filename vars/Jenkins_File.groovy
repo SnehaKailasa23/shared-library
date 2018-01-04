53c8cfe81d805dd52c442243f7cea5d1f15e0214
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-04 19:17:18 +0530

    Update Jenkins_File.groovy

--- vars/Jenkins_File.groovy
+++ vars/Jenkins_File.groovy
@@ -167,10 +167,10 @@ node {
 		} 
 		
 /****************************** Stage for creating reports for SonarQube Analysis ******************************/
-		stage ('Reports creation') {
+		/*stage ('Reports creation') {
 			Reason = "Reports creation Failed"
 			sh """ curl "http://10.240.17.12:9000/sonar/api/resources?resource=${Sonar_project_name}&metrics=bugs,vulnerabilities,code_smells,duplicated_blocks" > output.json """
-		} 
+		} */
 		
 /****************************** Stage for sending Email Notifications when Build succeeds ******************************/	
 		stage ('Email Notifications') {
