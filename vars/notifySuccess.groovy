2ce8069eea665421405f57f958586d19dfca22db
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-04 12:22:21 +0530

    Update notifySuccess.groovy

--- vars/notifySuccess.groovy
+++ vars/notifySuccess.groovy
@@ -1,9 +1,6 @@
 def call(def WORKSPACE) {
 properties([[$class: 'EnvInjectJobProperty', info: [loadFilesFromMaster: false, propertiesContent: "JobWorkSpace=${WORKSPACE}"], keepBuildVariables: true, keepJenkinsSystemVariables: true, on: true]])
- def Reason = "Sending Success email failed"
- emailext (
+emailext (
  attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''
- ${SCRIPT, template="email_template_success.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com, sneha.kailasa@ggktech.com, sunil.boga@ggktech.com'
- )
- return Reason
+ ${SCRIPT, template="email_template_success.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com, sneha.kailasa@ggktech.com, sunil.boga@ggktech.com' )
 }
