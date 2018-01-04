eef09b6c439188e9cc3f27f6e7ecc83d7ace889a
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-04 18:48:08 +0530

    Update TriggeringCDJob.groovy

--- vars/TriggeringCDJob.groovy
+++ vars/TriggeringCDJob.groovy
@@ -1,7 +1,5 @@
 def call(def Sonar_project_name, def CDEnvironment)
 {
-  println "inside CDTrigger"
   CD_Job_name = Sonar_project_name + CDEnvironment
-  println CD_Job_name
   build job: 'Docker_registry'//, parameters: [[$class: 'StringParameterValue', name: 'var1', value: 'var1_value']]
 }
