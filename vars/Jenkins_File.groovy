d0ca89a9985b09095a02c224562c1dc828ff0be0
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-04 18:56:01 +0530

    Update Jenkins_File.groovy

--- vars/Jenkins_File.groovy
+++ vars/Jenkins_File.groovy
@@ -1,14 +1,3 @@
-def call() {
-/****************************** Environment variables ******************************/  
-def JobName	= null						// variable to get jobname  
-def Sonar_project_name = null 							// varibale passed as SonarQube parameter while building the application
-def robot_result_folder = null 				// variable used to store Robot Framework test results
-def server = null							// Artifactory server instance declaration. 'server1' is the Server ID given to Artifactory server in Jenkins
-def buildInfo = null 						// variable to store build info which is used by Artifactory
-def rtMaven = Artifactory.newMavenBuild()	// creating an Artifactory Maven Build instance
-def Reason = "JOB FAILED"					// variable to display the build failure reason
-def lock_resource_name = null 					// variable for storing lock resource name
-
 // Reading jar file name from pom.xml //
 def getMavenBuildArtifactName() {
  pom = readMavenPom file: 'pom.xml'
@@ -30,6 +19,18 @@ emailext (
 	attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''${SCRIPT, template="email_template_failure.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com'
 	)
 }
+
+def call() {
+/****************************** Environment variables ******************************/  
+def JobName	= null						// variable to get jobname  
+def Sonar_project_name = null 							// varibale passed as SonarQube parameter while building the application
+def robot_result_folder = null 				// variable used to store Robot Framework test results
+def server = null							// Artifactory server instance declaration. 'server1' is the Server ID given to Artifactory server in Jenkins
+def buildInfo = null 						// variable to store build info which is used by Artifactory
+def rtMaven = Artifactory.newMavenBuild()	// creating an Artifactory Maven Build instance
+def Reason = "JOB FAILED"					// variable to display the build failure reason
+def lock_resource_name = null 					// variable for storing lock resource name
+
 
 /****************************** Jenkinsfile execution starts here ******************************/
 node {
