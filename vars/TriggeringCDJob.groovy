9f389dd388ca5c9d18d80d9c4ee2ea370965c911
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-04 11:01:46 +0530

    Update TriggeringCDJob.groovy

--- vars/TriggeringCDJob.groovy
+++ vars/TriggeringCDJob.groovy
@@ -1,8 +1,8 @@
-
-def call(def Sonar_project_name)
+def call(def Sonar_project_name, def CDJob)
 {
   def Reason = "Triggering CD job failed"
-  CD_Job_name = Sonar_project_name + "_QA"
+  CD_Job_name = Sonar_project_name + CDJob
+  println CD_Job_name
   build job: 'Docker_registry'//, parameters: [[$class: 'StringParameterValue', name: 'var1', value: 'var1_value']]
   return Reason
 }
